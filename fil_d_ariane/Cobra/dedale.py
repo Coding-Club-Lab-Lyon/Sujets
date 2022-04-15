55f2b47d0c97be9ef6341d027dcaebadb3eae31f
Mathias A <mathias.andre@epitech.eu>
2022-04-15 11:33:37 +0200

    [OTHER] Fil ariane subject.

--- fil_d_ariane/Cobra/dedale.py
+++ fil_d_ariane/Cobra/dedale.py
@@ -1,4 +1,71 @@
-from cc_maze.maze import *
+from tkinter import *
+
+incr = 2
+size = incr * 10
+posX = 1
+posY = 1
+map = [[]] * 100
+
+for value in range(0, 100):
+    map[value] = [0] * 100
+
+def checkered(canvas, line_distance):
+   # vertical lines at an interval of "line_distance" pixel
+   for x in range(line_distance,canvas_width,line_distance):
+      canvas.create_line(x * (incr), 0, x * (incr), canvas_height, fill="#476042")
+   # horizontal lines at an interval of "line_distance" pixel
+   for y in range(line_distance,canvas_height,line_distance):
+      canvas.create_line(0, y * (incr), canvas_width, y * (incr), fill="#476042")
+
+def right():
+    global posX
+    global posY
+    if (map[posY][posX + 1] == 1):
+        return
+    setACaseXY(posX, posY, 'grey')
+    map[posY][posX] = -1
+    posX += 1
+    setACaseXY(posX, posY, 'yellow')
+    if (map[posY][posX] == 2):
+        exit(0)
+
+def left():
+    global posX
+    global posY
+    if (map[posY][posX - 1] == 1):
+        return
+    setACaseXY(posX, posY, 'grey')
+    map[posY][posX] = -1
+    posX -= 1
+    setACaseXY(posX, posY, 'yellow')
+    if (map[posY][posX] == 2):
+        exit(0)
+
+def up():
+    global posY
+    global posX
+    if (map[posY - 1][posX] == 1):
+        return
+    setACaseXY(posX, posY, 'grey')
+    map[posY][posX] = -1
+    posY -= 1
+
+    setACaseXY(posX, posY, 'yellow')
+    if (map[posY][posX] == 2):
+        exit(0)
+
+def down():
+    global posY
+    global posX
+    if (map[posY + 1][posX] == 1):
+        return
+    setACaseXY(posX, posY, 'grey')
+    map[posY][posX] = -1
+    posY += 1
+    setACaseXY(posX, posY, 'yellow')
+    if (map[posY][posX] == 2):
+        exit(0)
+
 
 path = []
 
@@ -25,7 +92,21 @@ def algo(value):
         elif (path[-1] == "right"):
             left()
         path.pop()
-    print(path)
+
+
+def setACaseXY(X, Y, color):
+    points = [X * size, Y * size, size * (X + 1), Y * size, size * (X + 1), size * (Y + 1), X * size, size * (Y + 1)]
+    w.create_polygon(points, outline="#476042", fill=color, width=4)
+
+
+master = Tk()
+master.title("Le Fil d'Ariane")
+canvas_width = 1280
+canvas_height = 620
+w = Canvas(master,
+           width=canvas_width,
+           height=canvas_height)
+
 
 
 
@@ -54,4 +135,3 @@ for value in range(0, 1000):
 checkered(w,10)
 
 mainloop()
-
