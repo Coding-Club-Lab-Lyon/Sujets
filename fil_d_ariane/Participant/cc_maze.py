d157e3d707a58bd6e5af1a0f299efea3c0ee33fb
lg-epitech <laurent.gonzalez@epitech.eu>
2024-03-17 17:47:26 +0100

    [FIX] Sujet ariane participant

---
+++ fil_d_ariane/Participant/cc_maze.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+
+SQUARE_SIZE = 20
+DELAY = 300
+COLORS = {
+    "x": "black",
+    "-": "white",
+    "o": "green",
+    "P": "blue"
+}
+
+
+def load_from_file(path) -> (list[list[str]], int, int):
+    map: list[list[str]] = []
+
+    with open(path) as file:
+        for line in file.readlines():
+            map.append(list(line.rstrip()))
+    for i, row in enumerate(map):
+        for j, char in enumerate(row):
+            if char == "-":
+                return map, i, j
+    exit(84)
+
+
+def update_grid(canvas, map, PosY: int, PosX: int):
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            if PosY == i and PosX == j:
+                color = COLORS["P"]
+            else:
+                color = COLORS[map[i][j]]
+            x0 = j * SQUARE_SIZE
+            y0 = i * SQUARE_SIZE
+            x1 = x0 + SQUARE_SIZE
+            y1 = y0 + SQUARE_SIZE
+            canvas.create_rectangle(x0, y0, x1, y1, fill=color)
+    canvas.update()
+    canvas.after(DELAY)
+
+
+def init_graphics(map: list[list[str]], PosY, PosX):
+    width = len(map[0]) * SQUARE_SIZE
+    height = len(map) * SQUARE_SIZE
+    window = tk.Tk()
+    canvas = tk.Canvas(window, width=len(map[0]) * SQUARE_SIZE,
+                       height=len(map) * SQUARE_SIZE)
+
+    window.title("Participant: Fil d'Ariane")
+    window.geometry(f"{width}x{height}")
+    canvas.pack()
+    update_grid(canvas, map, PosY, PosX)
+    return window, canvas
--- fil_d_ariane/Participant/dedale.py
+++ fil_d_ariane/Participant/dedale.py
@@ -1,6 +1,48 @@
-from cc_maze import *
+import cc_maze as wrapper
+
+map, PosY, PosX = wrapper.load_from_file("map.txt")
+
+
+# Movement
+def up():
+    global map, PosY, PosX
+    if PosY != 0 and map[PosY - 1][PosX] != "x":
+        PosY -= 1
+
+
+def down():
+    global map, PosY, PosX
+    if PosY != len(map) - 1 and map[PosY + 1][PosX] != "x":
+        PosY += 1
+
+
+def left():
+    global map, PosY, PosX
+    if PosX != 0 and map[PosY][PosX - 1] != "x":
+        PosX -= 1
+
+
+def right():
+    global map, PosY, PosX
+    if PosX != len(map[PosY]) - 1 and map[PosY][PosX + 1] != "x":
+        PosX += 1
+
 
 path = []
 
-def algo(value):
-    down()
+
+def algo(canvas):
+    global map, PosY, PosX
+
+    # Votre algorithme va ici.
+    #
+    # Vous pouvez utiliser la fonction update_grid du module cc_maze
+    # pour mettre à jour l'affichage du labyrinthe dans votre algorithme.
+    # Exemple:
+    # wrapper.update_grid(canvas, map, PosY, PosX)
+
+
+if __name__ == "__main__":
+    window, canvas = wrapper.init_graphics(map, PosY, PosX)
+    algo(canvas)
+    window.mainloop()
