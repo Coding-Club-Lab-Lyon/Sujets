4caf06404b00f7528a5597ac8f93cd7f0aec46c1
Skignes <116216779+charlypal@users.noreply.github.com>
2025-07-09 22:12:29 +0200

    fix(voyageurs_des_plages): fix the main.c

---
+++ Voyageur_des_plages/Participant/main.py
@@ -0,0 +1,11 @@
+from beaches import beaches
+
+def display_beaches():
+    print("🏖️ PLAGES DISPONIBLES🏖️ " )
+    print("-" * 30)
+    # TODO: Afficher chaque plage avec ses coordonnées
+    pass
+
+
+if __name__ == "__main__":
+    display_beaches()
